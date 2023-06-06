542c5a4106514c0883ade693780a561980c982ec
zakelh6 <zakaria.elhajoui1@gmail.com>
2023-06-05 19:24:22 -0700

    Task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * check_cycle - a function in C that checks if a singly linked list
+ * has a cycle in it.
+ * @list: the list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+
+}
