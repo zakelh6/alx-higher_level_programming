c909fa7e7f359d5b909b2b118e438a510493d31a
zakelh6 <zakaria.elhajoui1@gmail.com>
2023-06-06 18:04:04 -0700

    TAsk 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10 
+last_digit = abs(number) % 10
 result = ""
 if number == 0:
     result = "is 0"
