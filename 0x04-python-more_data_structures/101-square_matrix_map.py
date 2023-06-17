00c40d30af85c602ed01449f9a2a8dcf0e1c810b
Zakaria elhajoui <zakaria.elhajoui1@gmail.com>
2023-06-17 03:02:12 +0100

    Task 15

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,2 @@
-#!/usr/bin/python3
-def square_matrix_map(matrix=[]):
+def complex_delete(a_dictionary, value):def square_matrix_map(matrix=[]):
     return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
