09b32f710665832cdb248044aef8d0816a7633a9
zakelh6 <zakaria.elhajoui1@gmail.com>
2023-06-30 02:08:02 +0100

    TAsk 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -10,11 +10,25 @@ class Square:
         Initializes Square class
         size: size of Square
         """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
+
+    @property  # getter
+    def size(self):
+        """
+        Returns size of Square
+        """
+        return self.__size
+    
+    @size.setter  # setter
+    def size(self, value):
+        """
+        Sets size of Square
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """
